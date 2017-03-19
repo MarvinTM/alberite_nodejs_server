49aaceefa957d229372e57b8147ed07ed7d56011
Antonio Moreno <anmorenoperez@gmail.com>
2017-03-19 13:21:52 +0000

    Added initial log storage functionality

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
+//DATABASE INITIALIZATION
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/alberite';
+
+var config = {
+  user: 'alberite', //env var: PGUSER 
+  database: 'alberite', //env var: PGDATABASE 
+  password: 'alberite', //env var: PGPASSWORD 
+  host: 'localhost', // Server hosting the postgres database 
+  port: 5432, //env var: PGPORT 
+  max: 10, // max number of clients in the pool 
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
+};
+
+var pool = new pg.Pool(config);
+function executeQuery(queryStr, success) {
+  pool.connect(function(err, client, done) {
+    var results = [];
+console.log('query string: '+queryStr);
+    var query = client.query(queryStr);
+    query.on('row', function (row) {
+      results.push(row);
+    });
+    query.on('end', function() {
+      done();
+      success(results);
+    });
+  });
+}
+//FINISHED DATABASE INITIALIZATION 
+
+
+
 var express = require('express');
 var app = express();
 var port = 8080;
 
 
-app.get('/test', function (req, res) {
-  res.send('Hello World!')
+app.get('/loginfo', function (req, res) {
+  var queryStr = 'SELECT id, message, messagedate, type FROM loginfo ORDER BY messagedate';
+  executeQuery(queryStr, function(results) {
+    res.json(results);
+  });
 })
 
 app.listen(port, function () {
