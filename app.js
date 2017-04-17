230ae0358d595339bb09abda9e1e3fb72bb40c7c
Antonio Moreno <anmorenoperez@gmail.com>
2017-04-17 18:39:50 +0000

    Added authentication

--- app.js
+++ app.js
@@ -53,6 +53,9 @@ function initiateSystemAction(pin, duration, callback) {
   executeQuery(insertStr, [pin, duration], callback); 
 };
 
+
+//EXPRESS INITIALIZATION
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
@@ -64,7 +67,111 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.get('/loginfo', function (req, res) {
+
+//AUTHENTICATION INITIALIZATION
+var auth = require('passport-local-authenticate');
+
+
+function insertUser(username, password, callback) {
+  console.log('Registering username: '+username+' with password: '+password);
+  auth.hash(password, function(err, hashed) {
+    var queryInsertUser = "INSERT INTO USERS VALUES ($1, $2, $3)";
+    console.log(err);
+    executeQuery(queryInsertUser, [username, hashed.hash, hashed.salt], function(results) {
+      callback();
+    });
+  });
+};
+
+function findUser(userObj, callback) {
+  var queryUser = "SELECT * FROM USERS WHERE name=$1";
+    executeQuery(queryUser, [userObj.username], function(results) {
+      if(results.length!=0) {
+        var hashed = {hash: results[0].password, salt: results[0].salt};
+        console.log('pass: '+userObj.password);
+        console.log(hashed.hash);
+        console.log(hashed.salt);
+        auth.verify(userObj.password, hashed, function(err, verified) {
+          if(verified) {
+            callback(null, userObj); 
+          } else {
+            callback('Wrong password', null);
+          }
+        });
+      } else {
+        callback('User not found', null);
+      }
+    });
+};
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    findUser({ username: username, password: password }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (false) { //!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+// Authentication middleware
+var session = require('express-session');
+
+app.use(express.static('public'));
+app.use(session({ secret: 'mourinho' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  //User.findById(id, function(err, user) {
+    done(null, user);
+  //});
+});
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/',
+                                   failureFlash: true })
+);
+
+app.get('/registerUser', ensureAuthenticated, function (req, res) {
+  var username = req.query.username;
+  var password = req.query.password;
+  insertUser(username, password, function() {
+    res.json({message: 'Everything is ok', opCode: 0}); 
+  });
+});
+
+app.get('/logout', ensureAuthenticated, function(req, res){
+  console.log('logging out');
+  req.logout();
+  res.redirect('/');
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    // req.user is available for use here
+    return next(); }
+
+  // denied. redirect to login
+  res.redirect('/')
+}
+
+
+// ENDPOINTS (APPLICATION)
+app.get('/loginfo', ensureAuthenticated, function (req, res) {
   var queryStr = 'SELECT id, message, messagedate, type, externalip FROM loginfo ORDER BY messagedate DESC LIMIT 20';
   executeQuery(queryStr, null, function(results) {
     res.json(results);
@@ -73,7 +180,7 @@ app.get('/loginfo', function (req, res) {
 
 app.post('/ping', function (req, res) {
   var bodyObj = req.body;
-console.log(JSON.stringify(bodyObj));
+  console.log(JSON.stringify(bodyObj));
   writeLogInfo(bodyObj, function(results) {
     var querySystem = "SELECT * FROM SYSTEM";
     executeQuery(querySystem, null, function(results) {
@@ -88,7 +195,7 @@ console.log(JSON.stringify(bodyObj));
   });
 });
 
-app.post('/systemInitiating',  function (req, res) {
+app.post('/systemInitiating', function (req, res) {
   var bodyObj = req.body;
   writeLogInfo(bodyObj, function(results) {
     //Delete system action from table
@@ -99,7 +206,7 @@ app.post('/systemInitiating',  function (req, res) {
   });
 });
 
-app.post('/systemHasStarted',  function (req, res) {
+app.post('/systemHasStarted', function (req, res) {
   var bodyObj = req.body;
   writeLogInfo(bodyObj, function(results) {
     var response = {message: 'Everything is ok', opCode: 0};
@@ -108,7 +215,7 @@ app.post('/systemHasStarted',  function (req, res) {
 });
 
 
-app.post('/systemHasFinished',  function (req, res) {
+app.post('/systemHasFinished', function (req, res) {
   var bodyObj = req.body;
   writeLogInfo(bodyObj, function(results) {
     var response = {message: 'Everything is ok', opCode: 0};
@@ -116,7 +223,7 @@ app.post('/systemHasFinished',  function (req, res) {
   });
 });
 
-app.get('/startSystemAction', function (req, res) {
+app.get('/startSystemAction', ensureAuthenticated, function (req, res) {
   var pin = req.query.pin;
   var duration = req.query.duration;
   console.log('Initiating action with pin: '+pin);
@@ -127,8 +234,18 @@ app.get('/startSystemAction', function (req, res) {
 
 
 //PAGES ENDPOINTS
-
 app.get('/', function(req, res) {
+  if (req.isAuthenticated()) {
+    res.redirect('/main'); 
+  } else {
+    fs.readFile('login.html', 'utf8', function(err, data) {
+      res.send(data);
+    });
+  }
+});
+
+
+app.get('/main', ensureAuthenticated, function(req, res) {
   fs.readFile('index.html', 'utf8', function(err, data) {
     res.send(data);
   });
